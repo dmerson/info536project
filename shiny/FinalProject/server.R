559b9a995a77edb2edf966e39f321b052e8183c0
don-E Merson <dmerson@email.arizona.edu>
2021-11-27 08:50:09 -0700

    Almost finished with final project.

--- shiny/FinalProject/server.R
+++ shiny/FinalProject/server.R
@@ -9,14 +9,62 @@
 
 library(shiny)
 library(tidyverse)
-ForStatisticalAnalysis <- read_csv("data/ForStatAnalysis.csv")
-# Define server logic required to draw a histogram
+library(DT)
+library(usmap)
+library(janitor)
+library(scales)
+data_for_plots <-read_csv("data/ForStatAnalysis.csv")
+data_for_table <- data_for_plots %>% select(-ElectionYear ) %>% select(-State_abb) %>% rename('Total Migration'=totalMigration)
+map_visualization <- data_for_plots %>% clean_names()
+migration_to_next_skew_model <- lm(skew2020 ~ totalMigration,
+                                   data = ForStatisticalAnalysis)
+migration_to_next_skew_model_summary=summary(migration_to_next_skew_model)
+migration_from_previous_election <- lm(skew2020 ~ skew2016 + totalMigration,
+                                       data = ForStatisticalAnalysis)
+migration_from_previous_election_summary <-summary(migration_from_previous_election)
+
+skew_from_previous_election <- lm(skew2020 ~ skew2016 ,
+                                       data = ForStatisticalAnalysis)
+skew_from_previous_election_summary <-summary(skew_from_previous_election)
+
 shinyServer(function(input, output) {
 
-    output$base_data <- renderDataTable({
-      ForStatisticalAnalysis
-         
-
-    })
+    output$base_data <- renderDataTable(
+      data_for_table,filter="top",
+      options=list(
+        pageLength = 10, scrollX=TRUE,  options = list(
+          columns = list(searchable = FALSE)
+        )
+      )
+    )
+    output$explain <-renderPlot(
+      plot_usmap(data = map_visualization, values = "difference", regions = "states") + 
+        scale_fill_manual(values = c("blue", "purple", "red"), name="Election") + 
+        labs(title = "2020 Election", subtitle = "2020 Election") +
+        theme(legend.position = "right")
+    )
+    output$firstmodel <- renderPrint(migration_to_next_skew_model_summary)
+    output$secondmodel <- renderPrint(migration_from_previous_election_summary)
+    output$thirdmodel <- renderPrint(skew_from_previous_election_summary)
+    output$explain2 <-renderPlot(
+        plot_usmap(data = map_visualization, values = "total_migration", regions = "states") + 
+                   scale_fill_continuous(low = "red", high = "blue", name = "Migration", label = scales::comma) + 
+                   labs(title = "U.S. Migration", subtitle = "Democratic-Republican Migration from other states") +
+                   theme(legend.position = "right")
+    )
+    output$linegraph <-renderPlot(ForStatisticalAnalysis %>% 
+                                    ggplot(aes(x=skew2016, y=skew2020, color=difference)) +
+                                    geom_point() +
+                                    geom_jitter() + 
+                                    geom_smooth(method="lm") +
+                                    geom_vline(xintercept = 0) +
+                                    geom_hline(yintercept = 0) +
+                                    scale_x_continuous(labels = comma) +
+                                    scale_y_continuous(labels=comma) +
+                                    scale_color_manual(values = c("Blue","Purple","Red"))  +
+                                    labs(title = "Skew 2016-2020",
+                                         subtitle = "Visualizing skew from the last 2 elections",
+                                         x = "2016 Skew",
+                                         y = "2020 Skew" ))
 
 })
--- shiny/FinalProject/ui.R
+++ shiny/FinalProject/ui.R
@@ -23,7 +23,7 @@ shinyUI(
         sidebarMenu(
           menuItem("Introduction", tabName = "Introduction",icon=icon("th")),
           menuItem('Data', tabName="Data", icon=icon("th")),
-          menuItem('Data Modeling', tabName="DataModeling", icon=icon("th")),
+          menuItem('Methods', tabName="DataModeling", icon=icon("th")),
           menuItem('Results', tabName="Results", icon=icon("th")),
           menuItem('Discussion', tabName="Discussion", icon=icon("th"))
         )
@@ -42,33 +42,77 @@ shinyUI(
                   elections having flipped many key states from usually red states to blue states,
                   it is possible that migration might have influenced these outcomes.  
                   This study looks to correlate migration from blue to red states to see
-                  if this might factor into the states being flipped in Presidential elections.")
+                  if this might factor into the states being flipped in Presidential elections."),
+                p("By looking at the map, we can see that 3 of the 5 states that changed sides border at least 2 blue states. Michigan is in the same region as Illinois and is very close. 
+                  Let's now look at the migration data. Remember that the migration data is Democratic-Republican, so blue means Democratic migration."),
+                plotOutput("explain2"),
+                p("Arizona and Pennsylvania show heavy migration from democratic states and switched during the election.
+                  This is not surprising because they each border 3 Democratic states in the 2016 election. 
+                  However, Georgia has a high level of Republican migration and it changed sides in 2020. 
+                  It looks like migration numbers are greatly influenced by proximity. For this study, we look to study these migration effects
+                  to see if they explain the 2020 election.")
                 ),
         tabItem(tabName = "Data",
                 h2("Data"),
                 p("The migration data for this study was pulled from the U.S. Census website at",
                 a(href="https://www.census.gov/data/tables/time-series/demo/geographic-mobility/state-to-state-migration.html","https://www.census.gov/data/tables/time-series/demo/geographic-mobility/state-to-state-migration.html"),
-                p("We looked to pull data from the years 2017,2018,2019, and 2020 to evaluate the 
+                "We looked to pull data from the years 2017,2018,2019, and 2020 to evaluate the 
                   results for the 2020 election. However, the 2020 data was not available at the 
                   time of the study. Due to the fact that 2020 was the year of COVID-19, 
                   these seemed acceptable because mobility was greatly constrained 
-                  due to the fact of lockdowns due to COVID-19. ")),
+                  due to the fact of lockdowns due to COVID-19. "),
                 p("The election data was pulled from",a(href="https://electionlab.mit.edu/data","https://electionlab.mit.edu/data")," for the U.S. Presidents from 2016-2020. 
                   This data contains the results for each presidential candidate for those years. "),
-                p("There was some data manipulation that was needed to get the data into a format to test our hypothesis. "),
+                p("Some data manipulation was needed to get the data into a format to test our hypothesis.  "),
+                p("The migration data needed to be pivoted and condensed into red and blue state migration.
+                  We used the year 2016 election to determine the state affiliation. We then summed up 
+                  the migration from Democratic states and Republican states into a variable called totalMigration.
+                  After this we created a variable named skew which was Democratic votes- Republic votes. 
+                  Therefore, a positive skew would mean Democratic migration while a negative value would 
+                  mean Republican migration.  We did this for 2016 and 2020, so we had a total skew for both elections.
+                  We also created a variable called difference which had a value of “RED-RED,” “RED-BLUE,” or “BLUE-BLUE,” 
+                  which describes the state’s group for our analysis. "),
+                p("You can see the data listed below in searchable form."),
+                dataTableOutput("base_data")
+               
+        ),
                 
-                dataTableOutput("base_data"),
                 
-                "Data"),
         tabItem(tabName = "DataModeling",
-                h2("Data Modeling"),
-                "Data Modeling"),
+                h2("Methods"),
+                p("The following map is a good visualization of what we are trying to explain. We want to see if we can explain the purple states.
+                  However, we want to explain them in a way that we don't get confused on why states didn't switch sides.  "),
+                plotOutput("explain"),
+                p("We looked to attempt to use linear regression to explain this phenomenon.")
+                
+                
+                
+        ),
         tabItem(tabName = "Results",
                 h2("Results"),
-                "Results"),
+                h3("First Model"),
+                p("I looked at my variable of migration in relation to 2020 skew."),
+                verbatimTextOutput ("firstmodel"),
+                p("This shows a positive relationship between migration and the 2020 skew. It is significant (p < .01)."),
+                p("However, the effect is minuscule with an Adjusted R-squared of 0.009984."),
+                h3("Second Model"),
+                p("The second model we used compared the 2016 skew and migration to 2020 skew."),
+                verbatimTextOutput ("secondmodel"),
+                p("In this case, we see that migration has a negative effect, but one of no significance.
+                  The previous year's skew is still a highly significant predictor of the next election's skew. It also shows that
+                  R-squared is very high at 98%!"),
+                p("This leads us to try a third model. We compared the the previous election's skew to the next election skew."),
+                h3("Third Model"),
+                verbatimTextOutput ("thirdmodel"),
+                p("In this model, we see that just the previous election's skew still explains 98% of the data according to the R-squared.")
+                ),
         tabItem(tabName = "Discussion",
                 h2("Discussion"),
-                "Discussion")
+                 p("From the results in this study, we conclude that migration does not have an effect on the changing of states in the 2020 election.
+                   We conclude that the biggest factor explained the results for the 2020 election is the 2016 election."),
+                plotOutput("linegraph")
+                
+                )
         
       )
     )
